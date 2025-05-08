a758ed36c00338eab2c0f75884bb41ae93d4b8f9
Akshay Bisht <akshaybisht544@gmail.com>
2025-05-08 07:29:19 -0400

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,5 +1,13 @@
+# A simple Python program that greets the user by their first and last name
+
+# Get user's first name input
 first_name = input("Enter your first name: ")
+
+# Get user's last name input
 last_name = input("Enter your last name: ")
 
+# Print a blank line for better formatting
 print("\n")
+
+# Print the welcome message by concatenating the input strings
 print("Hello, "+first_name +" "+ last_name+"! Welcome to the Python program.")
